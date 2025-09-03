62f1f68476300f99c5f0c54b297904b859f3f729
sjs <shan@gmail.com>
2025-09-03 14:32:05 +0530

    hello world program changed into Addition of two digits

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,10 @@
-public class Helloworld{
+public class Addition
+{
 public static void main(String[] args)
 {
-System.out.println("Hello World");
+int a=10,int b=20;
+int c=a+b;
+System.out.println(c);
 }
 
 }
