ffe2050e001313deacd48a9198b07a94bf519cca
sjs <shan@gmail.com>
2025-09-03 14:38:01 +0530

    Addition program changed into subtraction

--- Helloworld.java
+++ Helloworld.java
@@ -1,9 +1,9 @@
-public class Addition
+public class Subtraction
 {
 public static void main(String[] args)
 {
-int a=10,int b=20;
-int c=a+b;
+int a=20,int b=10;
+int c=a-b;
 System.out.println(c);
 }
 
